05c8d32a7b768df0ce10e464dc5e08cddabb2846
Nipuna <nipuna@app-monkeyz.com>
2017-05-20 15:15:04 +0530

    Create Deals page content

--- src/pages/deals-page/deals-page.ts
+++ src/pages/deals-page/deals-page.ts
@@ -13,12 +13,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'deals-page.html',
 })
 export class DealsPage {
+  rows: number[];
+  products: {}[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+this.products= [
+  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5},
+  {"id": 1, "title": "iPhone 4 ", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "Vader T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Backstreet Boys - Sucker CD", "price": 19.99, "inventory": 5},
+  {"id": 1, "title": "Gslaxy S7", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "Laptop", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5},
+  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5},
+  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5}
+];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DealsPage');
+    this.rows = Array.from(Array(Math.ceil(this.products.length / 2)).keys())
   }
 
 }
